b5b465225ac532880bfccc521c59bacee8e7b11d
Jonam-Mar <outwebsitecook@gmail.com>
2017-04-19 01:09:48 -0400

    First Upload

---
+++ JavaScript/ButtonScript.js
@@ -0,0 +1,8 @@
+function myFunction(y) {
+    var x = document.getElementById(y);
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
